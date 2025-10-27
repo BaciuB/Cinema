e956c081452f18b3cacebf9828fb029f254f95d0
BaciuB <bogdanbaciu762@gmail.com>
2025-10-28 00:00:33 +0200

    Staff gata

--- src/main/java/com/example/proiectul/Cinema/model/Staff.java
+++ src/main/java/com/example/proiectul/Cinema/model/Staff.java
@@ -3,4 +3,24 @@ package com.example.proiectul.Cinema.model;
 public abstract class Staff {
     private String Id;
     private String name;
+
+    public Staff() { }
+
+    public Staff(String id, String name) {
+        this.Id = id;
+        this.name = name;
+    }
+    public String getId() {return Id; }
+    public void setId(String id) { this.Id = id; }
+
+    public String getName() {return name;}
+    public void setName(String name) { this.name = name; }
+
+    @Override
+    public String toString() {
+        return "Staff{" +
+                "id = " + Id + '\'' +
+                ", name = '" + name + '\'' +
+                '}';
+    }
 }
