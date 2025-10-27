8755dee0b8137faf6a1bd8057d14a0e03de043c1
BaciuB <bogdanbaciu762@gmail.com>
2025-10-28 00:04:03 +0200

    SupportStaff gata

--- src/main/java/com/example/proiectul/Cinema/model/SupportStaff.java
+++ src/main/java/com/example/proiectul/Cinema/model/SupportStaff.java
@@ -2,5 +2,20 @@ package com.example.proiectul.Cinema.model;
 
 public class SupportStaff extends Staff {
     private String role;
+
+    public SupportStaff() { }
+
+    public SupportStaff(String Id, String name, String role) {
+        super(Id, name);
+        this.role = role;
+    }
+
+    public String getRole() {return role; }
+    public void setRole(String role) { this.role = role; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", role = '" + role + "'";
+    }
 }
 
