21fd3e79854d853b890022fa2c76a137172b2468
BaciuB <bogdanbaciu762@gmail.com>
2025-10-27 23:27:53 +0200

    Hall gata

--- src/main/java/com/example/proiectul/Cinema/model/Hall.java
+++ src/main/java/com/example/proiectul/Cinema/model/Hall.java
@@ -9,5 +9,32 @@ public class Hall {
     private int capacity;
     private List<Seat> seats;
     private List<Screening> screenings;
+
+    public Hall() { }
+
+    public Hall(String id, String name, String theatreId, int capacity) {
+        this.id = id;
+        this.name = name;
+        this.theatreId = theatreId;
+        this.capacity = capacity;
+    }
+
+    public String getId() {return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getTheatreId() { return theatreId; }
+    public void setTheatreId(String theatreId) { this.theatreId = theatreId; }
+
+    public int getCapacity() { return capacity; }
+    public void setCapacity(int capacity) { this.capacity = capacity; }
+
+    public List<Seat> getSeats() { return seats; }
+    public void setSeats(List<Seat> seats) { this.seats = seats; }
+
+    public List<Screening> getScreenings() { return screenings; }
+    public void setScreenings(List<Screening> screenings) { this.screenings = screenings; }
 }
 
