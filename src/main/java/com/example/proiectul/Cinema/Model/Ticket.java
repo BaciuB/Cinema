d044eac25720d334e628e51b9368565ffd41da27
BaciuB <bogdanbaciu762@gmail.com>
2025-10-27 22:17:20 +0200

    am facut inca un pic

---
+++ src/main/java/com/example/proiectul/Cinema/Model/Ticket.java
@@ -0,0 +1,14 @@
+package com.example.proiectul.Cinema.Model;
+
+import java.time.LocalDateTime;
+
+public class Ticket {
+    private Long id;
+    private Movie movie;
+    private Customer customer;
+    private LocalDateTime screeningTime;
+    private double price;
+
+
+}
+
