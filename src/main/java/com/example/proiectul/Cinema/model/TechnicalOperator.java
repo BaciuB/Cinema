f3afc52e65fc650d7da75ffeeb934a98377c7947
BaciuB <bogdanbaciu762@gmail.com>
2025-10-28 00:09:13 +0200

    TechnicalOperator gata

--- src/main/java/com/example/proiectul/Cinema/model/TechnicalOperator.java
+++ src/main/java/com/example/proiectul/Cinema/model/TechnicalOperator.java
@@ -2,5 +2,20 @@ package com.example.proiectul.Cinema.model;
 
 public class TechnicalOperator extends Staff {
     private String specialization;
+
+    public TechnicalOperator() { }
+
+    public TechnicalOperator( String Id, String name, String specialization) {
+        super(Id, name);
+        this.specialization = specialization;
+    }
+
+    public String getspecialization() {return specialization; }
+    public void setspecialization(String specialization) {this.specialization = specialization;}
+
+    @Override
+    public String toString() {
+        return super.toString() + ", specialization = '" + specialization +"'";
+    }
 }
 
