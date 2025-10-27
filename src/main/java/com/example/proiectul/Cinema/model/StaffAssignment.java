8f70b797b4e3c717dc7b0f40788506d662f25948
BaciuB <bogdanbaciu762@gmail.com>
2025-10-28 00:14:14 +0200

    StaffAssignment gata

--- src/main/java/com/example/proiectul/Cinema/model/StaffAssignment.java
+++ src/main/java/com/example/proiectul/Cinema/model/StaffAssignment.java
@@ -4,4 +4,30 @@ public class StaffAssignment {
     private String Id;
     private String screeningId;
     private String staffId;
+
+    public StaffAssignment() { }
+
+    public StaffAssignment(String Id, String screeningId, String staffId) {
+        this.Id = Id;
+        this.screeningId = screeningId;
+        this.staffId = staffId;
+    }
+
+    public String getId() {return Id; }
+    public void setId(String Id) { this.Id = Id; }
+
+    public String getScreeningId() { return screeningId; }
+    public void setScreeningId(String screeningId) { this.screeningId = screeningId; }
+
+    public String getStaffId() { return staffId; }
+    public void setStaffId(String staffId) { this.staffId = staffId; }
+
+    @Override
+    public String toString() {
+        return "StaffAssignment{" +
+                "id = '" + Id + '\'' +
+                ", screeningId = '" + screeningId + '\'' +
+                ", staffId = '" + staffId + '\'' +
+                '}';
+    }
 }
