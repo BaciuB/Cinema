eab64d4b8aa35ef8d23c68c1c78178960746fb45
BaciuB <bogdanbaciu762@gmail.com>
2025-11-18 01:00:31 +0200

    cred ca e ok 0.2

---
+++ src/main/java/com/example/proiectul/Cinema/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.proiectul.Cinema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
