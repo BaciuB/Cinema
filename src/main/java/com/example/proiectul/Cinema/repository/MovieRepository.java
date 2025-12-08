1688696fda5a921b9aae26ee8fed80798369591b
BaciuB <bogdanbaciu762@gmail.com>
2025-12-08 22:11:56 +0200

    merge acu ceva

--- src/main/java/com/example/proiectul/Cinema/repository/MovieRepository.java
+++ src/main/java/com/example/proiectul/Cinema/repository/MovieRepository.java
@@ -9,6 +9,4 @@ public interface MovieRepository extends JpaRepository<Movie, String> {
     boolean existsByTitle(String title);
 
     boolean existsByTitleAndIdNot(String title, String id);
-
-
 }
